4ac0985c9f9590aa0d91b2ef3d39306888e8a7c2
gunatejapm <62699029+gunatejapm@users.noreply.github.com>
2021-12-31 14:54:48 +0530

    Update print.java

--- print.java
+++ print.java
@@ -1,6 +1,6 @@
 Class Example333 {
   public static void main(String args[]) {
     System.out.println("This program is from GitHub");
-    System.out.println("Overwritinf file for testing purpose");
+    System.out.println("Overwriting file for testing purpose");
     }
 }
